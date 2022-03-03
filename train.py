b20ce171cad852d947565077d555778c0309fe8b
yerang <97432613+yerang823@users.noreply.github.com>
2022-03-03 14:56:58 +0900

    Update train.py

--- train.py
+++ train.py
@@ -169,17 +169,13 @@ beta = 0.6
   
 def recall(y_target, y_pred):
     # clip(t, clip_value_min, clip_value_max) : clip_value_min~clip_value_max 이외 가장자리를 깎아 낸다
-    # round : 반올림한다
     y_target_yn = K.round(K.clip(y_target, 0, 1)) # 실제값을 0(Negative) 또는 1(Positive)로 설정한다
     y_pred_yn = K.round(K.clip(y_pred, 0, 1)) # 예측값을 0(Negative) 또는 1(Positive)로 설정한다
 
-    # True Positive는 실제 값과 예측 값이 모두 1(Positive)인 경우이다
     count_true_positive = K.sum(y_target_yn * y_pred_yn) 
 
-    # (True Positive + False Negative) = 실제 값이 1(Positive) 전체
     count_true_positive_false_negative = K.sum(y_target_yn)
 
-    # Recall =  (True Positive) / (True Positive + False Negative)
     # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
     recall = count_true_positive / (count_true_positive_false_negative + K.epsilon())
 
@@ -189,17 +185,12 @@ def recall(y_target, y_pred):
 
 def precision(y_target, y_pred):
     # clip(t, clip_value_min, clip_value_max) : clip_value_min~clip_value_max 이외 가장자리를 깎아 낸다
-    # round : 반올림한다
     y_pred_yn = K.round(K.clip(y_pred, 0, 1)) # 예측값을 0(Negative) 또는 1(Positive)로 설정한다
     y_target_yn = K.round(K.clip(y_target, 0, 1)) # 실제값을 0(Negative) 또는 1(Positive)로 설정한다
-
-    # True Positive는 실제 값과 예측 값이 모두 1(Positive)인 경우이다
     count_true_positive = K.sum(y_target_yn * y_pred_yn) 
 
-    # (True Positive + False Positive) = 예측 값이 1(Positive) 전체
     count_true_positive_false_positive = K.sum(y_pred_yn)
-
-    # Precision = (True Positive) / (True Positive + False Positive)
+    
     # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
     precision = count_true_positive / (count_true_positive_false_positive + K.epsilon())
 
